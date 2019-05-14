a3292f8be219121c27a0617090dbd083eeeb5a16
Gowtham <gowthamsm26@gmail.com>
2019-05-14 18:39:09 +0530

    add form for messages and incoming

--- public/js/index.js
+++ public/js/index.js
@@ -6,15 +6,26 @@ var socket = io();
       
     });
 
-    // socket.emit('createMessage', {
-    //     from: 'gowthamsm26@gmail.com',
-    //     text: 'Hey, good to go'
-    // });
     
     socket.on('disconnect', function() {
       console.log('Disconnected from server');
     });
 
     socket.on('newMessage', function(message){
-        console.log('newMessage', message);
+      console.log('newMessage', message);
+      var li = jQuery('<li></li>');
+      li.text(`${message.from}: ${message.text}`);
+
+      jQuery('#messages').append(li);
+    });
+
+    jQuery('#message-form').on('submit', function (e){
+      e.preventDefault();
+
+      socket.emit('createMessage', {
+        from: 'user',
+        text: jQuery('[name=message]').val()
+      }, function(){
+
+      })
     });
